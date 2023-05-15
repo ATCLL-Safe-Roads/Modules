ad0924e5bfa7a493cf60f04bd2e8ecebe5bdf63f
João Fonseca <joao.fonseca@ua.pt>
2023-05-16 00:21:04 +0100

    PP-41 simplify API docs and fix docker compose

--- API/app/main.py
+++ API/app/main.py
@@ -6,51 +6,30 @@ from app.mqtt import mqtt
 from app.routers import events, flows, graphs
 
 app = FastAPI()
-
-# Connect to MQTT broker
-
 mqtt.init_app(app)
 
 
-# Add OpenAPI tags
 def custom_openapi():
-    if app.openapi_schema:
-        return app.openapi_schema
-    openapi_schema = get_openapi(
-        title="ATCLL-Data",
-        version="1.0.0",
-        description="ATCLL-Data API",
+    return get_openapi(
+        title='ATCLL - Safe Roads - API Module',
+        version='1.0.0',
         routes=app.routes,
     )
-    openapi_schema["info"]["x-logo"] = {
-        "url": "https://atcll-data.nap.av.it.pt/static/img/logo.png"
-    }
-    # Add Description to parameter start in endpoint /events/get_events_filtered
-    openapi_schema["paths"]["/events/get_events_filtered"]["get"]["parameters"][3][
-        "description"] = "Start date in format YYYY-MM-DD"
-    # Add Description to parameter end in endpoint /events/get_events_filtered
-    openapi_schema["paths"]["/events/get_events_filtered"]["get"]["parameters"][4][
-        "description"] = "End date in format YYYY-MM-DD"
-    # Add Description to parameter start in endpoint /flows/get_flows_filtered
-    openapi_schema["paths"]["/flows/get_flows_filtered"]["get"]["parameters"][1][
-        "description"] = "Start date in format YYYY-MM-DD"
-    # Add Description to parameter end in endpoint /flows/get_flows_filtered
-    openapi_schema["paths"]["/flows/get_flows_filtered"]["get"]["parameters"][2][
-        "description"] = "End date in format YYYY-MM-DD"
-    app.openapi_schema = openapi_schema
-    return app.openapi_schema
 
 
+# API Documentation
 app.openapi = custom_openapi
 
+# API CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=['*'],
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=['*'],
+    allow_headers=['*'],
 )
 
-app.include_router(events.router, prefix="/events", tags=["events"])
-app.include_router(flows.router, prefix="/flows", tags=["flows"])
+# API Routes
+app.include_router(events.router, prefix='/events', tags=['events'])
+app.include_router(flows.router, prefix='/flows', tags=['flows'])
 app.include_router(graphs.router, prefix='/graphs', tags=['graphs'])
--- API/app/mqtt.py
+++ API/app/mqtt.py
@@ -7,7 +7,6 @@ from app.database import Event, Flow
 from app.config import settings
 
 mqtt_config = MQTTConfig(host=settings.ATCLL_BROKER_HOST, port=settings.ATCLL_BROKER_PORT)
-
 mqtt = FastMQTT(config=mqtt_config)
 
 ide = 0
@@ -16,46 +15,46 @@ idf = 0
 
 @mqtt.on_connect()
 def connect(client, flags, rc, properties):
-    mqtt.client.subscribe("/events")
-    mqtt.client.subscribe("/traffic-flow-here")
-    mqtt.client.subscribe("/flows")
-    print("Connected: ", client, flags, rc, properties)
+    mqtt.client.subscribe('/events')
+    mqtt.client.subscribe('/traffic-flow-here')
+    mqtt.client.subscribe('/flows')
+    print('INFO: Connected to MQTT Broker')
 
 
 @mqtt.on_message()
 async def message(client, topic, payload, qos, properties):
-    if topic == "/events":
+    if topic == '/events':
         global ide
-        if properties["retain"] == 1:
+        if properties['retain'] == 1:
             return
         r = payload.decode()
         j = json.loads(r)
-        s = datetime.strptime(j["start"], '%Y-%m-%dT%H:%M:%S%z')
-        j["start"] = s
-        e = datetime.strptime(j["end"], '%Y-%m-%dT%H:%M:%S%z')
-        j["end"] = e
-        j["id"] = ide
+        s = datetime.strptime(j['start'], '%Y-%m-%dT%H:%M:%S%z')
+        j['start'] = s
+        e = datetime.strptime(j['end'], '%Y-%m-%dT%H:%M:%S%z')
+        j['end'] = e
+        j['id'] = ide
         ide += 1
         Event.insert_one(j)
-    if topic == "/traffic-flow-here":
+    if topic == '/traffic-flow-here':
         global idf
-        if properties["retain"] == 1:
+        if properties['retain'] == 1:
             return
         r = payload.decode()
         j = json.loads(r)
-        t = datetime.strptime(j["timestamp"], '%Y-%m-%dT%H:%M:%S%z')
-        j["timestamp"] = t
-        j["id"] = idf
+        t = datetime.strptime(j['timestamp'], '%Y-%m-%dT%H:%M:%S%z')
+        j['timestamp'] = t
+        j['id'] = idf
         idf += 1
         Flow.insert_one(j)
-    if topic == "/flows":
-        if properties["retain"] == 1:
+    if topic == '/flows':
+        if properties['retain'] == 1:
             return
         r = payload.decode()
         j = json.loads(r)
-        t = datetime.strptime(j["timestamp"], "%Y-%m-%d %H:%M:%S.%f%z")
-        j["timestamp"] = t
-        j["id"] = idf
+        t = datetime.strptime(j['timestamp'], '%Y-%m-%d %H:%M:%S.%f%z')
+        j['timestamp'] = t
+        j['id'] = idf
         idf += 1
         Flow.insert_one(j)
-    print("Received message: ", topic, payload.decode(), qos, properties)
+    print(f'INFO: Received message on topic {topic} {payload.decode()[:100]}')
